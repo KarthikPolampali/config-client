bbdb778616cfd81e536b6397d9acc80412d07c22
Karthik Polampalli <karthik.polampalli96@gmail.com>
2022-11-02 00:03:01 +0530

    Decryption and Refresh

--- src/main/java/com/kp/configclient/RateController.java
+++ src/main/java/com/kp/configclient/RateController.java
@@ -1,10 +1,12 @@
 package com.kp.configclient;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RefreshScope
 @Controller
 public class RateController {
 
@@ -14,12 +16,15 @@ public class RateController {
     String lanecount;
     @Value("${tollstart}")
     String tollstart;
+    @Value("${connstring}")
+    String connstring;
 
     @RequestMapping("/rate")
     public String getRate(Model m){
         m.addAttribute("rateamount", rate);
         m.addAttribute("lanes", lanecount);
         m.addAttribute("tollstart", tollstart);
+        m.addAttribute("connstring", connstring);
 
         return "rateview";
     }
